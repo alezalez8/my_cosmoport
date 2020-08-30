3c873afa8d51ac9f29a66f86df36a310c944d3ab
alezalez8 <5446943@ukr.net>
2020-08-30 17:58:12 +0300

    delete repo with old code

--- space/src/main/java/com/space/repository/ShipRepo.java
+++
@@ -1,8 +0,0 @@
-package com.space.repository;
-
-import com.space.model.Ship;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShipRepo extends JpaRepository<Ship, Long> {
-
-}
